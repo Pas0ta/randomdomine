2d25e1f716a01ce27edcf81a0988cae6af9da67c
Pas0ta <ssantamariarodrigo@gmail.com>
2023-01-08 15:20:10 +0000

    final version

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // datos a generar
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let final = [".com", ".es", ".org"];
+
+  // zona aleatoria
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let t = 0; t < adj.length; t++) {
+      for (let m = 0; m < noun.length; m++) {
+        for (let p = 0; p < final.length; p++)
+          console.log(pronoun[i] + noun[t] + adj[m] + final[p]);
+      }
+    }
+  }
 };
